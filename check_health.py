bc0dc8f2820dcadaaa199fbbe64fa547c9da9960
zahrabateninia <zahrabatenin@gmail.com>
2023-10-29 11:57:43 +0330

    Rename min_absolute to min_gb, use parameter names

--- check_health.py
+++ check_health.py
@@ -3,19 +3,19 @@
 import shutil
 import sys
 
-def check_disk_usage(disk, min_absolute, min_percent):
+def check_disk_usage(disk, min_gb, min_percent):
     """Returns True if there is enough free disk space, false otherwise."""
     du = shutil.disk_usage(disk)
     # Calculate the percentage of free space
     percent_free = 100 *du.free / du.total
     # Calculate how many free gigabytes
     gigabytes_free = du.free/ 2**30  # a gigabyte is equal to 2**30 bytes
-    if percent_free < min_percent or gigabytes_free < min_absolute:
+    if percent_free < min_percent or gigabytes_free < min_gb:
         return False 
     return True 
 
 # Check for at least 2 gigabytes and 10% free
-if not check_disk_usage("/",2,10):
+if not check_disk_usage(disk="/",min_gb=2,min_percent=10):
     print("ERROR: Not enough disk space")
     sys.exit(1)
 
